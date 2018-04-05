3625b20ecf267b62617e852e5e3decf26844234a
JamisonOrdway <jamisonordway@gmail.com>
2018-04-05 11:51:37 -0600

    Adds reunion tests

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,4 +1,15 @@
 class Reunion
+  attr_accessor :location,
+                :activities
 
-  
+  def initialize(location)
+    @location = location
+    @activities = {"Skiing"=>200}
+  end
+
+  def add_activity(name, cost)
+    @activities[name] = cost
+  end
+
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -1,7 +1,28 @@
 require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/reunion'
+require 'pry'
 
 class ReunionTest < Minitest::Test
+
+  def setup
+    @reunion = Reunion.new("Zermatt")
+  end
+
+  def test_it_has_a_location
+    assert_equal "Zermatt", @reunion.location
+  end
+
+  def test_it_has_activities
+    expected = {"Skiing"=>200}
+    assert_equal expected, @reunion.activities
+  end
+
+  def test_activities_can_be_added
+    expected = {"Skiing"=>200, "Snowboarding"=>200}
+    assert_equal expected, @reunion.add_activity("Snowboarding", 200)
+  end
+
 end
