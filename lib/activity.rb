9c6ab4f374167f0b8b2ec8ed37af19c72c75938a
JamisonOrdway <jamisonordway@gmail.com>
2018-04-05 10:24:22 -0600

    Adds activity tests

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,16 @@
 class Activity
+  attr_reader :name,
+              :cost
+  attr_accessor :participants
+
+  def initialize(name, cost)
+    @name = name
+    @cost = cost
+    @participants = {}
+  end
+
+  def add_participant(name, amount_paid)
+    
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,28 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def setup
+    @activity = Activity.new("snowboarding", 200)
+  end
+
+  def test_it_has_a_name
+    assert_equal "snowboarding", @activity.name
+  end
+
+  def test_it_has_a_cost
+    assert_equal 200, @activity.cost
+  end
+
+  def test_it_starts_with_no_participants
+    expected = {}
+    assert_equal expected, @activity.participants
+  end
+
+  def test_it_can_add_participants
+    @activity.add_participant("Shaun", 50)
+    expected = {"Shaun" => 50}
+    assert_equal expected, @activity.participants
+  end
+
 end
